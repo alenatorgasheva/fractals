c33d846eac0b26e28711cbb40699a9c4101f595a
alenatorgasheva <in-fa@mail.ru>
2020-02-10 22:41:48 +0700

    Update menu.

--- main.py
+++ main.py
@@ -5,21 +5,11 @@
 
 # This program draws fractals.
 
-# ОСТАЛЬНЫЕ ЗАДАЧИ:
-#   1. код ревью + комментарии
-#   2. меню программы (выбор фигуры и ее параметров)
-#   3. ?локализация
-
 from turtle import *
 
 
 # Двоичное дерево (спрашивает высоту дерева и величину угла)
 def tree():
-    pass
-
-
-# Ветка
-def brunch():
     pass
 
 
@@ -54,7 +44,25 @@ def minkowski(deep, length):
 
 
 # Ледяной фрактал 1
-def ice_1():
+def ice_1(deep, length):
+    if deep == 0:
+        forward(length)
+    else:
+        ice_1(deep - 1, length)
+        left(120)
+        ice_1(deep - 1, length / 2)
+        left(180)
+        ice_1(deep - 1, length / 2)
+        left(120)
+        ice_1(deep - 1, length / 2)
+        left(180)
+        ice_1(deep - 1, length / 2)
+        left(120)
+        ice_1(deep - 1, length)
+
+
+# Ледяной фрактал 1, снежинка
+def ice_1_snowflake():
     pass
 
 
@@ -63,11 +71,15 @@ def ice_2():
     pass
 
 
+# Ледяной фрактал 2, снежинка
+def ice_2_snowflake():
+    pass
+
+
 # Кривая Леви
 def levi(order, size):
     if order == 0:
         forward(size)
-
     else:
         left(45)
         levi(order - 1, size / 1.5)
@@ -76,49 +88,23 @@ def levi(order, size):
         left(45)
 
 
-
-# Дракон
-def dragon():
-    pass
-
-
 def main():
     print('Выберите фигуру из списка.')
     print('\t1 - двоичное дерево')
-    print('\t2 - ветка')
-    print('\t3 - кривая Коха')
-    print('\t4 - снежинка Коха')
-    print('\t5 - ледяной фрактал 1')
-    print('\t6 - ледяной фрактал 2')
-    print('\t7 - кривая Минковского')
-    print('\t8 - кривая Леви')
-    print('\t9 - дракон')
+    print('\t2 - кривая Леви')
+    print('\t3 - кривая Минковского')
+    print('\t4 - кривая Коха')
+    print('\t5 - снежинка Коха')
+    print('\t6 - ледяной фрактал 1')
+    print('\t7 - ледяной фрактал 1, снежинка')
+    print('\t8 - ледяной фрактал 2')
+    print('\t9 - ледяной фрактал 2, снежинка')
     choice = input()
 
     if choice == '1':
         tree()
 
     elif choice == '2':
-        brunch()
-
-    elif choice == '3':
-        koch()
-
-    elif choice == '4':
-        koch_snowflake()
-
-    elif choice == '5':
-        ice_1()
-
-    elif choice == '6':
-        ice_2()
-
-    elif choice == '7':
-        deep = int(input('Глубина: '))
-        length = int(input('Длина стороны: '))
-        minkowski(deep, length)
-
-    elif choice == '8':
         order = int(input('Глубина: '))
         size = int(input('Длина стороны: '))
         up()
@@ -127,8 +113,28 @@ def main():
         levi(order, size)
         done()
 
+    elif choice == '3':
+        deep = int(input('Глубина: '))
+        length = int(input('Длина стороны: '))
+        minkowski(deep, length)
+
+    elif choice == '4':
+        koch()
+
+    elif choice == '5':
+        koch_snowflake()
+
+    elif choice == '6':
+        ice_1()
+
+    elif choice == '7':
+        ice_1_snowflake()
+
+    elif choice == '8':
+        ice_2()
+
     elif choice == '9':
-        dragon()
+        ice_2_snowflake()
 
 
 main()
